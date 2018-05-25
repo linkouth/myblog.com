d64b0fe2e423be281fd9df9cca069caab0396135
linkouth <dkatimullin@gmail.com>
2018-05-25 17:43:19 +0200

    Hello Username

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,9 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
+        echo '<h1>Hello Usename!<h1>';
+        exit();
+        
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
